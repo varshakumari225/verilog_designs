ae3f8e4dc6e64d7c6d079fc402ae41596f839467
varshakumari225 <gokanavarsha@gmail.com>
2025-10-10 14:38:13 +0530

    Update design.v

--- half_adder_design_using_different_modelling_styles/design.v
+++ half_adder_design_using_different_modelling_styles/design.v
@@ -7,7 +7,7 @@ module half_adder(input a,b,output df_s,df_c,st_s,st_c,output reg be_s,be_c);
   xor(st_s,a,b);
   and(st_c,a,b);
   //behavioural
-  always@(*) begin *is implication operator
+  always@(*) begin //*is implication operator
     be_s=a^b;
     be_c=a&b;
   end
