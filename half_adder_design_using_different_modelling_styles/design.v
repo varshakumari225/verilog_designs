63caff60caf1eda3d13acc62d3e4759662ebf140
varshakumari225 <gokanavarsha@gmail.com>
2025-10-10 14:29:03 +0530

    Create design.v

---
+++ half_adder_design_using_different_modelling_styles/design.v
@@ -0,0 +1,14 @@
+module half_adder(input a,b,output df_s,df_c,st_s,st_c,output reg be_s,be_c);
+  //sum=a^b carry=ab
+  //data flow
+  assign df_s=(a^b);
+  assign df_c=a&b;
+  //structural
+  xor(st_s,a,b);
+  and(st_c,a,b);
+  //behavioural
+  always@(*) begin *is implication operator
+    be_s=a^b;
+    be_c=a&b;
+  end
+endmodule
