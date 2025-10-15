8b75f72cd1260dcabab6bdb1abfffc3a9e8a1ff8
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2025-10-15 14:26:48 +0530

    Update DESIGN.v

--- FLIP_FLOPS/D_FF/DESIGN.v
+++ FLIP_FLOPS/D_FF/DESIGN.v
@@ -1,4 +1,5 @@
 module d_ff(input rst,clk,d,output reg q);
+  //Behavioural logic
   always @(posedge clk) begin
   //When reset = 1 → circuit resets
   //When reset = 0 → circuit works normally
@@ -7,4 +8,11 @@ module d_ff(input rst,clk,d,output reg q);
     else
       q<=d;
   end
+  //Gate level 
+  wire w1,w2,w3,w4;
+  not(w4,d);
+  nand(q,w2,w1);
+  nand(w1,w3,q);
+  nand(w2,d,clk);
+  nand(w3,w4,clk);
 endmodule
