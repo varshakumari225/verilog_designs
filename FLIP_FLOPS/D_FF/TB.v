4b9e85d7dbb2d0c0de7acf9ac1662e0c33c052ff
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2025-10-16 11:56:30 +0530

    Update TB.v

--- FLIP_FLOPS/D_FF/TB.v
+++ FLIP_FLOPS/D_FF/TB.v
@@ -9,7 +9,7 @@ module tb;
     $dumpfile("d_ff.vcd");
     $dumpvars(0,tb);
       rst=1;d=1;#5;
-      rst=0;d=0;#5;
+      rst=0;d=0;#5; //for Asynchronous reset change delay to 6 means the rst change after 6ns of delay.
       d=1;#5;
       d=1;#5;
     $finish;
