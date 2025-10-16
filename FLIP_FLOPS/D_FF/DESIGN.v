48e13a8624ed9d61fb33fb7270fb46d2a3405f7e
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2025-10-16 11:52:36 +0530

    Change D flip-flop to asynchronous reset logic
    Refactor D flip-flop to use asynchronous reset.

--- FLIP_FLOPS/D_FF/DESIGN.v
+++ FLIP_FLOPS/D_FF/DESIGN.v
@@ -1,8 +1,11 @@
 module d_ff(input rst,clk,d,output reg q);
   //Behavioural logic
-  always @(posedge clk) begin
-  //When reset = 1 → circuit resets
-  //When reset = 0 → circuit works normally
+  //synchronous reset
+  //always @(posedge clk) begin
+ /* When reset = 1 → circuit resets
+  When reset = 0 → circuit works normally*/
+  //asynchronous reset
+    always @(posedge clk or posedge rst)begin
     if (rst) 
       q<=0;
     else
