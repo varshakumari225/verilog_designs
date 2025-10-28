9b9aeff8eacdc21faaf9dcd0b709acddd9642bf4
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2025-10-28 17:44:59 +0530

    Add testbench for twisted ring counter

---
+++ COUNTERS/SYNCHRONOUS_COUNTERS/TWISTED_RING_COUNTER/TB.v
@@ -0,0 +1,18 @@
+module tb;
+  reg clk,clr;
+  reg [3:0] d;
+  wire [3:0] q,qbar; //q[3]q[2]q[1]q[0]
+  jc u(clk,clr,d,q,qbar);
+  initial clk=0;
+  always #5 clk=~clk;
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    $monitor("at time %t clr=%b d=%b q=%b",$time,clr,d,q);
+    clr=1;#10;
+    clr=0;
+    d=4'b1;
+    #100;
+    $finish;
+  end
+endmodule
