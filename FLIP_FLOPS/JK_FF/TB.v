0ec0b7cd07014a5b9687002b54d8263f9c0f0c0f
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2025-10-28 10:46:55 +0530

    Implement JK flip-flop test bench
    Added a test bench for the JK flip-flop with clock generation and input tasks.

--- FLIP_FLOPS/JK_FF/TB.v
+++ FLIP_FLOPS/JK_FF/TB.v
@@ -16,3 +16,27 @@ module tb;
     $finish;
   end
 endmodule
+//test bench by using the tasks
+module jk_tb;
+  reg clk,j,k;
+  wire q;
+  jk u(.clk(clk),.j(j),.k(k),.q(q));
+  initial clk=0;
+  always #5 clk=~clk;
+  task in(input t,input u);
+    begin
+    #10;
+    j=t;
+    k=u;
+    end
+  endtask
+  initial begin
+    $monitor("at time %t the value of j=%b k=%b q=%b",$time,j,k,q);
+    in(0,0);
+    in(0,1);
+    in(1,0);
+    in(1,1);
+    #10;
+    $finish;
+  end
+endmodule
